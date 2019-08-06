d3b40778ba2fb8d473c76e8b54791304d6ee8931
diegopereiracalcadadev <diegopereiracalcada@gmail.com>
2019-08-06 01:08:19 -0300

    Implementada exibição dos detalhe de uma receita ao clicar nela

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent   {
+
+  @Input() recipe: Recipe;
+  @Output() recipeClicked = new EventEmitter<{recipe: Recipe}>();
 
   constructor() { }
 
-  ngOnInit() {
+  showDetails(event){
+    this.recipeClicked.emit({recipe: this.recipe});
   }
-
 }
